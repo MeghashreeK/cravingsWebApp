a9338aed6e1b07af439cea2f1c367ee0dd0f424b
MeghashreeK <kundermeghashree279@gmail.com>
2024-06-17 12:27:16 +0530

    deployed proxyserver link added

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,6 +1,6 @@
 export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 export const LOGO_URL="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQp5KrJv-iYMPuxeMFGtdjZKobv20IUFu1fg&usqp=CAU";
 // export const MENU_URL="https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D17.3935543%26lng%3D78.6039862%26restaurantId%3D";
-export const MENU_URL="http://localhost:8080/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.96340&lng=77.58550&restaurantId=";
+export const MENU_URL="https://cravingsproxyserver.onrender.com/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.96340&lng=77.58550&restaurantId=";
 
 
